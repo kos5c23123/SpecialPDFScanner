2d40e3ec29d5cf29c7b82964a16726e6ac96f892
DannyTan <tantszhang1@gmail.com>
2024-08-22 21:50:10 -0400

    add a docker

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def download_file(filename):
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
